72ec17cc51bc17768590d7abf0e432849b9afb08
Toissincera <uzmanqaisar12@gmail.com>
2024-12-10 10:55:27 +0530

    Platformer changes

---
+++ src/pages/platformer/Level1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { levelOneLayout, tileToRender } from "./Level1Backstage";
+
+export default function Level1() {
+  const top = levelOneLayout[0];
+  const base = levelOneLayout[1];
+  return (
+    <div className="d-flex flex-column">
+      <div className="d-flex myLevel">
+        {top.map((tileType, ix) => tileToRender(tileType, ix))}
+      </div>
+      <div className="d-flex myLevel">
+        {base.map((tileType, ix) => tileToRender(tileType, ix))}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/platformer/Level1Backstage.jsx
@@ -0,0 +1,73 @@
+import "./Level1Backstage.css";
+
+const M = "player";
+const I = "wall";
+const X = "lava";
+const _ = "platform";
+const $ = "coin";
+const O = "empty";
+
+export const levelOneLayout = [
+  [I, I, $, O, O, O, M, O, O, $, O, $, I, I],
+  [_, _, _, _, X, X, _, _, X, X, _, _, _, _],
+];
+
+export const tiles = {
+  player: (ix) => (
+    <i
+      key={ix}
+      class="bi bi-person-standing fs-1 text-center"
+    ></i>
+  ),
+  wall: (ix) => (
+    <div
+      key={ix}
+      className="wall"
+    ></div>
+  ),
+  lava: (ix) => (
+    <div
+      key={ix}
+      className="lava"
+    ></div>
+  ),
+  platform: (ix) => (
+    <div
+      key={ix}
+      className="platform"
+    ></div>
+  ),
+  coin: (ix) => (
+    <div
+      key={ix}
+      className="coinOuter"
+    >
+      <div className="coinInner"></div>
+    </div>
+  ),
+  empty: (ix) => (
+    <div
+      key={ix}
+      className="empty"
+    ></div>
+  ),
+};
+
+export function tileToRender(tileType, ix) {
+  switch (tileType) {
+    case M:
+      return tiles.player(ix);
+    case I:
+      return tiles.wall(ix);
+    case X:
+      return tiles.lava(ix);
+    case _:
+      return tiles.platform(ix);
+    case $:
+      return tiles.coin(ix);
+    case O:
+      return tiles.empty(ix);
+    default:
+      return tiles.empty(ix);
+  }
+}
